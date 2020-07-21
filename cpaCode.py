159da4393096b9ad5e125c4a659074d18610319f
JessicaL1397 <47909646+JessicaL1397@users.noreply.github.com>
2020-07-21 10:56:28 -0400

    Ah. Jus the beginnings.

---
+++ cpaCode.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+    Cipher.txt = 7000 rows vs 16 columns
+    Plaintext.txt = 7000 rows vs 3125 columns
+
+    Read them in like a list. It's a 2D list. Should be 16 and then 7000 things inside each thing
+'''
+if __name__ == "__main__":
+    with open("ciphers.txt", "r") as f:
+        ciphers[] = f.read();
+    with open("plaintexts.txt", "r")
+        plainText =
+
+    cipherArray = array.ones(7000, 16)
+    plainTextArray = array.ones(7000, 3125)
+
+    for x in range(7000)
